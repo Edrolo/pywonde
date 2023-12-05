430552aceaa8de59f69b99e4b2314ece221e1ef8
David Fernandez <i.am.david.fernandez@gmail.com>
2023-12-05 11:50:07 +1100

    Modified generation to use docker.

--- meta/generate-python-nextgen.sh
+++ meta/generate-python-nextgen.sh
@@ -4,11 +4,20 @@
 
 #export PYTHON_POST_PROCESS_FILE="ruff"
 
-openapi-generator generate \
-  --generator-name=python-nextgen \
-  --config=openapi-generator-config.json \
-  --input-spec=wonde.openapi.yaml \
-  --output=..
+# openapi-generator generate \
+#   --generator-name=python-nextgen \
+#   --config=openapi-generator-config.json \
+#   --input-spec=wonde.openapi.yaml \
+#   --output=..
+# #  --enable-post-process-file \
+
+docker run --rm \
+  -v ${PWD}/..:/project \
+  openapitools/openapi-generator-cli generate \
+  --generator-name=python \
+  --config=/project/meta/openapi-generator-config.json \
+  --input-spec=/project/meta/wonde.openapi.yaml \
+  --output=/project
 #  --enable-post-process-file \
 
 poetry run pre-commit run --all-files
