8ca881e0e419f986bc05d1582aa09970c12c3da4
David Fernandez <i.am.david.fernandez@gmail.com>
2023-12-18 17:08:44 +1100

    Schema tweaks.

--- meta/generate-python-nextgen.sh
+++ meta/generate-python-nextgen.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 # Generate the Python client from the OpenAPI specification,
-# using the Java-based openapi-generator tool with the python-nextgen generator.
+# using the docker-based openapi-generator tool with the python generator.
 
 #export PYTHON_POST_PROCESS_FILE="ruff"
 
-# openapi-generator generate \
-#   --generator-name=python-nextgen \
-#   --config=openapi-generator-config.json \
-#   --input-spec=wonde.openapi.yaml \
-#   --output=..
-# #  --enable-post-process-file \
+# GENERATOR_VERSION=v6.6.0
+GENERATOR_VERSION=v7.0.1
 
+# Note: We will run the container as the current user/group, so that the generated files
+#       are owned by the current user, not by root.
 docker run --rm \
+  --user $(id --user):$(id --group) \
   -v ${PWD}/..:/project \
-  openapitools/openapi-generator-cli generate \
+  openapitools/openapi-generator-cli:${GENERATOR_VERSION} generate \
   --generator-name=python \
   --config=/project/meta/openapi-generator-config.json \
   --input-spec=/project/meta/wonde.openapi.yaml \
   --output=/project
 #  --enable-post-process-file \
 
-poetry run pre-commit run --all-files
+# poetry run pre-commit run --all-files
+pre-commit run --all-files
